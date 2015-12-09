62aa915ffbec1e743b00be6a69f11970b7f2e9de
Afdel Abdollah <aa28g15@soton.ac.uk>
2015-12-09 17:29:14 +0000

    Query 7

--- test.py
+++ test.py
@@ -127,3 +127,37 @@ countBigram = db.microblog.map_reduce(map6_2, reduce6, "myresults6_2")
 for bigram in countBigram.find().sort('value', pymongo.DESCENDING).limit(10):
         print "Bigram : ",bigram.get("_id")
 	print "Count : ",bigram.get("value")
+
+
+# 8
+
+#pipeline8 = [
+#        {"$group": {"_id": { "lat": "$geo_lat", "long": "$geo_lng" }, "count": {"$sum": 1 } } },
+#        {"$sort": SON([("count", -1)])}
+#        ]
+
+pipeline8 = [
+		{ "$match": 	{"$and" : [ { "geo_lat" : {'$gte': 50, '$lte': 55 } } , { "geo_lng" : {'$gte': 0 , '$lte': 4 } } ] } },
+		{ "$group": { "_id": { "lat": "$geo_lat", "long": "$geo_lng" }, "count": { "$sum": 1 } } }, 
+		{ "$sort": { "count": -1 } }, { "$limit": 1 } 
+        ]
+
+command = db.command('aggregate', 'microblog', pipeline=pipeline8, allowDiskUse=True)
+
+resultArray = command.get("result")
+
+result = resultArray[0]
+
+coord = result.get("_id")
+
+latitude = coord.get("lat")
+
+longitude = coord.get("long")
+
+coordMsgCount = result.get("count")
+
+print " The Region in the UK with the highest number of messages is : Latitude : ",latitude," Longitude : ",longitude
+print " Number of messages is : ",coordMsgCount
+
+#for count8 in list(db.microblog.aggregate(pipeline8)):
+#	print count8
