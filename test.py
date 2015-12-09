0295d9044d30622415bf0a1d31b94c7fff1405df
Afdel Abdollah <aa28g15@soton.ac.uk>
2015-12-09 18:27:51 +0000

    Query 7

--- test.py
+++ test.py
@@ -129,6 +129,30 @@ for bigram in countBigram.find().sort('value', pymongo.DESCENDING).limit(10):
 	print "Count : ",bigram.get("value")
 
 
+# 7
+
+map7 = Code("function () {"
+        "       	var text = String(this.text);                  "
+        "       	emit('hashtags', ( text.match(/\#/g) || []).length );"
+        "       }"
+)
+
+reduce7 = Code("function (key, values) {"
+               "  var total = 0;"
+               "  var count = 0;"
+               "  for (var i = 0; i < values.length; i++) {"
+               "    total += values[i];"
+               "   count += 1;        "
+               "  }"
+               "  return total/count;"
+               "}")
+
+averageHashTag = db.microblog.map_reduce(map7, reduce7, "myresults")
+
+
+for average in averageHashTag.find():
+        print " Mean of Hash Tags within a message",average.get("value")
+
 # 8
 
 #pipeline8 = [
